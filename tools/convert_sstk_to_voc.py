f951d06e16c13953ac6d5804fbd01ff69fa17c0e
Caglayan (Cha) Dicle <cdicle@shutterstock.com>
2017-09-19 10:53:33 -0400

    Fix naming in conversion script

--- tools/convert_sstk_to_voc.py
+++ tools/convert_sstk_to_voc.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Converts SSTK type single annotation file to Pascal VOC \
         type multiple files')
-    parser.add_argument('--input_file', default=None,
-                        help='path to input for SSTK type annotation file(default=None)')
-    parser.add_argument('--output_folder', default=None,
-                        help='path to output folder for Pascal VOC type annotations(default=None)')
+    parser.add_argument('--input_file', required=True,
+                        help='path to input for SSTK type annotation file')
+    parser.add_argument('--output_folder', required=True,
+                        help='path to output folder for Pascal VOC type annotations')
 
     args = parser.parse_args()
     input_file = args.input_file
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         annotations = json.load(fp)
 
     for image_name in annotations.keys():
-        print(image_name,len(annotations[image_name]))
+        print('Converting ' + image_name)
 
         def convert(sstk):
             x1, y1, x2, y2 = sstk['bbox']
@@ -32,10 +32,9 @@ if __name__ == "__main__":
             voc['fill_color']=None
             voc['difficult']=False
             return voc
-            
+
         sstk_annotation=annotations[image_name]
         voc_annotation=[convert(item) for item in sstk_annotation]
-
 
         json_name = os.path.splitext(image_name)[0] + '.json'
         with open(os.path.join(output_folder, json_name), 'w') as fp:
